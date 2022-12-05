1e649ab7b4765665ed1a42217245c85d9ffa5e06
nnhuy20130281 <20130281@st.hcmuaf.edu.vn>
2022-12-05 10:19:16 +0700

    them chuc nang dang xuat

---
+++ src/main/java/vn/edu/hcmuaf/fit/adminController/AdminController.java
@@ -0,0 +1,23 @@
+package vn.edu.hcmuaf.fit.adminController;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+
+@WebServlet(name = "AdminController", value = "/admin/AdminController")
+public class AdminController extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doPost(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        HttpSession session = request.getSession();
+        if (session != null) {
+            session.invalidate();
+            response.sendRedirect("../login.jsp");
+        }
+    }
+}
