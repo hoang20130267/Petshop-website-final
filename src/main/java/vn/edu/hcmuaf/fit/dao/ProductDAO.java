b062854261312af2e3d76d40b05aa2b90ac85345
hoang20130267 <20130267@st.hcmuaf.edu.vn>
2022-12-14 13:48:42 +0700

    làm chức năng thêm sửa xóa sản phẩm cho admin

--- src/main/java/vn/edu/hcmuaf/fit/dao/ProductDAO.java
+++ src/main/java/vn/edu/hcmuaf/fit/dao/ProductDAO.java
@@ -3,7 +3,6 @@ package vn.edu.hcmuaf.fit.dao;
 import vn.edu.hcmuaf.fit.beans.Product;
 import vn.edu.hcmuaf.fit.db.JDBIConnector;
 
-import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -13,6 +12,7 @@ public class ProductDAO {
     private List<Product> listProductCat;
     private List<Product> listProductAccessory;
     private List<String> listCategory;
+    private List<String> listCategoryAdmin;
 
     public ProductDAO(){
         listProduct = JDBIConnector.get().withHandle(handle -> {
@@ -35,6 +35,10 @@ public class ProductDAO {
             return handle.createQuery("select distinct giong from product having giong != \"null\" ")
                     .mapTo(String.class).stream().collect(Collectors.toList());
         });
+        listCategory = JDBIConnector.get().withHandle(handle -> {
+            return handle.createQuery("select * from product_category")
+                    .mapTo(String.class).stream().collect(Collectors.toList());
+        });
     }
     public List<Product> searchByName(String txtSearch) {
         List<Product> list = JDBIConnector.get().withHandle(handle -> {
@@ -50,9 +54,55 @@ public class ProductDAO {
         });
         return list;
     }
+    public static Product getProductById(String id){
+        Product p = JDBIConnector.get().withHandle(handle -> {
+            return handle.createQuery("select * from product where productId = ?")
+                    .bind(0, id).mapToBean(Product.class).one();
+        });
+        return p;
+    }
+    public static void deleteProduct(String id) {
+        JDBIConnector.get().withHandle(handle -> {
+            return handle.createUpdate("delete from product where productId = ?")
+                    .bind(0, id)
+                    .execute();
+        });
+    }
+    public static void insertProduct(String id, String name, String image, String price,
+                              String description, String giong, String cannang) {
+        JDBIConnector.get().withHandle(handle -> {
+            return handle.createUpdate("insert into product (productId, ProductName, Image, Price, Description, giong, cannang) values(?,?,?,?,?,?,?)")
+                    .bind(0, id)
+                    .bind(1, name)
+                    .bind(2, image)
+                    .bind(3, price)
+                    .bind(4, description)
+                    .bind(5, giong)
+                    .bind(6, cannang)
+                    .execute();
+        });
+    }
+    public static void updateProduct(String id, String name, String image, String price,
+                              String description, String giong, String cannang) {
+        JDBIConnector.get().withHandle(handle -> {
+            return handle.createUpdate("update product set ProductName = ?, Image = ?, Price = ?, Description = ?, giong = ?, cannang = ? where productId = ?")
+                    .bind(6, id)
+                    .bind(0, name)
+                    .bind(1, image)
+                    .bind(2, price)
+                    .bind(3, description)
+                    .bind(4, giong)
+                    .bind(5, cannang)
+                    .execute();
+        });
+    }
     public static void main(String[] args) {
         new ProductDAO();
-        System.out.println(new ProductDAO().searchByName("ALASKA"));
-        System.out.println(new ProductDAO().listCategory);
+//        System.out.println(new ProductDAO().searchByName("ALASKA"));
+//        System.out.println(new ProductDAO().listCategory);
+//        deleteProduct("1000");
+          updateProduct("1000", "Cho mat ngao", "deo co", "20000000", "cho ngu vcl", "cho ngu", "5kg");
+//        insertProduct("1000", "Cho mat ngu", "deo co", "10000000", "cho ngu vcl", "cho ngu", "5kg");
+        System.out.println(new ProductDAO().searchByName("Cho mat ngu"));
     }
 }
