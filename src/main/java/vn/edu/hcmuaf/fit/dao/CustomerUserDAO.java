ef6e6f9be169a45fa672686bcb9bd2fb7971fe75
Nguyễn Huy Hoàng <110890529+HuyHoang-20130266@users.noreply.github.com>
2022-12-17 20:35:27 +0700

    testt

--- src/main/java/vn/edu/hcmuaf/fit/dao/CustomerUserDAO.java
+++ src/main/java/vn/edu/hcmuaf/fit/dao/CustomerUserDAO.java
@@ -99,10 +99,11 @@ public class CustomerUserDAO {
     }
 
     public List<CustomerUser> listUser() {
-        String query = "SELECT u.user_name, u.id,ifu.email, od.Quantity, od.Price\n" +
+        String query = "SELECT u.user_name, u.id,ifu.email, SUM(od.Price) as Price, SUM(od.Quantity) AS Quantity \n" +
                 "FROM user_account u join infor_user ifu on u.id = ifu.id_user \n" +
                 "join orders o on o.CustomerID = ifu.id_user\n" +
-                "join orderdetail od on o.OrderID = od.OrderID;\n";
+                "join orderdetail od on o.OrderID = od.OrderID\n" +
+                "GROUP BY u.user_name, u.id;\n";
         String FinalQuery = query;
         List<CustomerUser> list = JDBIConnector.get().withHandle(handle -> handle.createQuery(FinalQuery)
                 .mapToBean(CustomerUser.class)
