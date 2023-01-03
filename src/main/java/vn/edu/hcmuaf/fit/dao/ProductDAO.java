a8acc7bc17db8c34fe56b18bc6769af2183a9014
hoang20130267 <20130267@st.hcmuaf.edu.vn>
2023-01-03 19:41:58 +0700

    fix

--- src/main/java/vn/edu/hcmuaf/fit/dao/ProductDAO.java
+++ src/main/java/vn/edu/hcmuaf/fit/dao/ProductDAO.java
@@ -431,18 +431,14 @@ public class ProductDAO {
         // int amount, String category,String price, String size, String order_by
 //        System.out.println(new ProductDAO().Test(9,"cat","20000000-200000000","0-2","3"));
 
-<<<<<<< HEAD
-
 //          System.out.println(new ProductDAO().getNext9Product(0,"all","10000000-150000000","2-5","2"));
 
 //        System.out.println(new ProductDAO().getNext9Product(0,"1","10000000-150000000","2-5","2"));
 //        System.out.println(new ProductDAO().getFullProduct("all","10000000-150000000","2-5","2"));
 
-=======
 //          System.out.println(new ProductDAO().getNext9Product(0,"all","10000000-150000000","2-5","2"));
   //        System.out.println(new ProductDAO().getNext9Product(0,"1","10000000-150000000","2-5","2"));
         System.out.println(new ProductDAO().getFullProduct("all","10000000-150000000","2-5","2"));
->>>>>>> 82fb8f7fe7d1c5d8eeb4da07613834150f0750a2
 //          System.out.println(new ProductDAO().getNext9Product(0,"dog","10000000-150000000","2-5","2"));
 
 //        System.out.println(new ProductDAO().test("dog"));
