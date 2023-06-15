bd601cb9c02baf05ff6ee3b2339a245c39ce1e04
Nguyễn Huy Hoàng <110890529+HuyHoang-20130266@users.noreply.github.com>
2023-06-16 00:24:07 +0700

    sửa cái phân trang có ... cho đẹp ở product với blog

---
+++ src/main/java/vn/edu/hcmuaf/fit/controller/PaggingBlogs.java
@@ -0,0 +1,18 @@
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+
+@WebServlet(name = "PaggingBlogs", value = "/PaggingBlogs")
+public class PaggingBlogs extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
